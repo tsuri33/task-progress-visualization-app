ed8080df37a0fd7913d77a317c229323d17ad65f
Tsuri Yuma <yuma33t@gmail.com>
2023-07-05 14:46:31 +0900

    Initial Commit

---
+++ TaskProgressVisualization/TaskProgressVisualizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskProgressVisualizationApp.swift
+//  TaskProgressVisualization
+//
+//  Created by 釣悠馬 on 2023/07/05.
+//
+
+import SwiftUI
+
+@main
+struct TaskProgressVisualizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
