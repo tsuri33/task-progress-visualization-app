a4004e5cd5e53722ca523f890bf826e2a1b115da
Tsuri Yuma <yuma33t@gmail.com>
2023-12-27 15:54:12 +0900

    Realmの処理を追加

--- TaskProgressVisualization/Models.swift
+++ TaskProgressVisualization/Models.swift
@@ -1,19 +1,47 @@
 import Foundation
+import RealmSwift
 
-class RealmHelper {
-    func save() {
-        
+struct RealmHelper {
+    
+    let realm = try! Realm()
+    
+    func save(model: Object, action: () -> Void) {
+        action()
+        try! realm.write {
+            realm.add(model)
+        }
     }
     
-    func read() {
-        
+    func read<T: Object>(modelType: T.Type) {
+        let modelTable = realm.objects(modelType)
+        print(modelTable)
     }
     
-    func update() {
-        
+    func update(model: Object, updateParts: () -> Void) {
+        updateParts()
+        try! realm.write {
+            realm.add(model, update: .modified)
+        }
     }
     
-    func delete() {
-        
+    func deleteTable(model: Object) {
+        try! realm.write {
+            realm.delete(model)
+        }
+    }
+    
+    func deletePart<T: Object>(modelType: T.Type, conditions: @escaping (T) -> Bool) {
+        do {
+            let realm = try Realm()
+            if let delete = realm.objects(modelType).first(where: { conditions($0) }) {
+                try! realm.write {
+                    realm.delete(delete)
+                }
+            } else {
+                print("条件に一致するオブジェクトが見つかりませんでした。")
+            }
+        } catch let error as NSError {
+            print("Realm 初期化エラーまたは削除エラー: \(error.localizedDescription)")
+        }
     }
 }
