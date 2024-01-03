cb74b6e3032d95fdca19e25159a60ea145263e4f
Tsuri Yuma <yuma33t@gmail.com>
2024-01-03 23:10:44 +0900

    Realmのデータを扱うモデルを変更

--- TaskProgressVisualization/Models.swift
+++ TaskProgressVisualization/Models.swift
@@ -1,47 +1,48 @@
 import Foundation
+import Combine
 import RealmSwift
 
-struct RealmHelper {
-    
-    let realm = try! Realm()
-    
-    func save(model: Object, action: () -> Void) {
-        action()
-        try! realm.write {
-            realm.add(model)
-        }
-    }
-    
-    func read<T: Object>(modelType: T.Type) {
-        let modelTable = realm.objects(modelType)
-        print(modelTable)
-    }
-    
-    func update(model: Object, updateParts: () -> Void) {
-        updateParts()
-        try! realm.write {
-            realm.add(model, update: .modified)
-        }
-    }
-    
-    func deleteTable(model: Object) {
-        try! realm.write {
-            realm.delete(model)
-        }
-    }
-    
-    func deletePart<T: Object>(modelType: T.Type, conditions: @escaping (T) -> Bool) {
+class RealmHelper {
+
+    private var realm: Realm {
         do {
-            let realm = try Realm()
-            if let delete = realm.objects(modelType).first(where: { conditions($0) }) {
-                try! realm.write {
-                    realm.delete(delete)
-                }
-            } else {
-                print("条件に一致するオブジェクトが見つかりませんでした。")
+            return try Realm()
+        } catch {
+            fatalError("Error initializing Realm: \(error.localizedDescription)")
+        }
+    }
+
+    func add(_ name: String) {
+        let newTask = Task()
+        newTask.name = name
+
+        do {
+            try realm.write {
+                realm.add(newTask)
             }
-        } catch let error as NSError {
-            print("Realm 初期化エラーまたは削除エラー: \(error.localizedDescription)")
+        } catch {
+            print("Error adding YourObject: \(error.localizedDescription)")
+        }
+    }
+
+    func update(_ task: Task, name: String) {
+        do {
+            try realm.write {
+                task.name = name
+            }
+        } catch {
+            print("Error updating YourObject: \(error.localizedDescription)")
+        }
+    }
+
+    func delete(_ task: Task, deleteID: ObjectId) {
+        let deleteData = realm.objects(Task.self).filter("id == \(deleteID)")
+        do {
+            try realm.write {
+                realm.delete(deleteData)
+            }
+        } catch {
+            print("Error deleting YourObject: \(error.localizedDescription)")
         }
     }
 }
