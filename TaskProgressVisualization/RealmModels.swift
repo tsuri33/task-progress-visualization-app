583020c92de25d3b6cf2a8f47618dcb5e1ed7366
Tsuri Yuma <yuma33t@gmail.com>
2024-01-03 23:11:06 +0900

    Realmモデルを変更

--- TaskProgressVisualization/RealmModels.swift
+++ TaskProgressVisualization/RealmModels.swift
@@ -5,11 +5,15 @@ class Task: Object, Identifiable {
     @Persisted(primaryKey: true) var id: ObjectId
     @Persisted var name:String
     @Persisted var amount:Int
-    @Persisted var amountToAdvancePerDay:Int
+    @Persisted var completedAmount:Int
+    @Persisted var advanceAmountPerDay:Int
     @Persisted var lastDate:Date
-    @Persisted var period:Int
+    @Persisted var deadline:Int
     @Persisted var numberDoTask:Int
-    @Persisted var isProgressing: Bool
+    @Persisted var isProgressing:Bool
+    @Persisted var progressValue:Double
+    @Persisted var rateOfAchievement:Int
+    @Persisted var selectionDate:Date
 }
 
 class TaskSelectionDate: Object {
